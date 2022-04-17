9219560120fc8bbc4b93f9e1105605889fb60a68
JetStream-Deiv <103295543+JetStream-Deiv@users.noreply.github.com>
2022-04-17 12:32:35 -0300

    Add files via upload

---
+++ grafico3.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x1 = (1, 5, 7, 10)
+y1 = (3, 15, 21, 30)
+
+plt.plot([1, 5, 7, 10], [3, 15, 21, 30])
+plt.title('peso x altura')
+plt.ylabel('altura')
+plt.xlabel('peso')
+plt.show()
+
+"""1) Faça um gráfico de linhas com as variáveis: 
+x2 = [1,5,7,10] e y2 = [3,15,21,30] e coloque nome no eixo X ("peso") eixo Y ("altura" e no título coloque Peso X Altura"""
