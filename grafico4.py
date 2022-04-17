cc25ba2a4db5cf189437bbf6006285f9f604522c
JetStream-Deiv <103295543+JetStream-Deiv@users.noreply.github.com>
2022-04-17 12:43:10 -0300

    Add files via upload

---
+++ grafico4.py
@@ -0,0 +1,13 @@
+import matplotlib.pyplot as plt
+
+x1 = (1, 2, 4, 6)
+y1 = (3, 7, 9, 15)
+
+plt.plot(x2, y2)
+plt.ylabel('dados')
+plt.xlabel('peso')
+plt.title('dados x altura')
+plt.show()
+
+"""Faça um gráfico de linhas com as variáveis: 
+x2 = [1,2,4,6] e y2 = [3,7,9,15] e coloque nome no eixo X ("dados") eixo Y ("peso") e no título coloque dados x peso"""
