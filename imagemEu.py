ab946bd4d98a8473e1535551eadad1d4c1f98878
JetStream-Deiv <103295543+JetStream-Deiv@users.noreply.github.com>
2022-04-17 12:53:10 -0300

    Add files via upload

---
+++ imagemEu.py
@@ -0,0 +1,12 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+import cv2
+
+from google.colab import files
+upload = files.upload()
+
+im=plt.imread('me.jpg')
+plt.imshow(im)
+
+"""Carregue uma foto sua no python (deixe o eixo)"""
