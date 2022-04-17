16a7dbbfa304acea9346d16541bccb6d2893ecc2
JetStream-Deiv <103295543+JetStream-Deiv@users.noreply.github.com>
2022-04-17 12:19:22 -0300

    Add files via upload

---
+++ grafico1.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+x1 = (1, 3, 5, 7, 9, 11)
+y1 = (6, 8, 10, 12, 14, 20)
+
+plt.plot(x1, y1)
+plt.show()
+
+
+"""1) Faça um gráfico de linhas e um gráfico relacionando os dados abaixo: [
+a) X1 = [1, 3, 5, 7, 9, 11] Y1 = [6,8,10,12,14,20]"""
