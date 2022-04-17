539e3d239e51bdac0499facf621d08abce1f6956
JetStream-Deiv <103295543+JetStream-Deiv@users.noreply.github.com>
2022-04-17 12:49:52 -0300

    Add files via upload

---
+++ grafico5.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as plt
+
+x4 = (2, 5, 7, 9)
+y4 = (4, 10, 14, 18)
+
+plt.bar(x4, y4, color = "green")
+plt.show()
+
+"""Faça um gráfico de barra x4 = (2,5,7,9) y4 = (4,10,14,18) 
+Coloque as barras na cor verde."""
