46e13f9aa37b06fb0a63f5333914fb3fb9bb2ca0
JetStream-Deiv <103295543+JetStream-Deiv@users.noreply.github.com>
2022-04-20 10:42:36 -0300

    Add files via upload

--- 1grau.py
+++ 1grau.py
@@ -1,14 +1,27 @@
 import matplotlib.pyplot as plt
+import tkinter as tk
+from tkinter import simpledialog
 
-A = float(input("Insira o valor de (A): "))
+# Em cima está importado o tkinter, e embaixo está criando o objeto "WINDOW".
+WINDOW = tk.Tk()
+# Para remover a interface da tela do usuário sem "destruí-la"
+WINDOW.withdraw()
+# Dialogo de INPUT para o usuario
+A = simpledialog.askfloat(title="Valores da Funcao",
+                          prompt="Insira o valor de (A): ")
 
-B = float(input("Insira o valor de (B): "))
+B = simpledialog.askfloat(title="Valores da Funcao",
+                          prompt="Insira o valor de (B): ")
 
-X = float(input("insira o valor de X: "))
+X = simpledialog.askfloat(title="Valores da Funcao",
+                          prompt="Insira o valor de (X): ")
+# Metodo askfloat é apenas para pegar o valor numerico inserido
 
 Y = (A * X) + B
 
-print("valor do calculo: ", Y)
+# para testar se a conta está correta.
+# No print abaixo, também foi utilizado o f string
+print(f'valor do calculo: {Y:.2f}')
 
 x1 = (X, 0)
 y1 = (Y, 0)
